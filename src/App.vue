b8a3683ab51446b9b42695ff10251d114147927f
tomotakashimizu <tomotaka.developer@gmail.com>
2021-02-12 19:56:16 +0900

    テストの追記

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     {{ text }}
+    {{ test }}
   </div>
 </template>
 
@@ -9,6 +10,7 @@ export default {
   data() {
     return {
       text: "Hello",
+      test: "テストです。",
     }
   },
 }
