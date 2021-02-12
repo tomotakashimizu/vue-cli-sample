468a37f9aad27334266d8f939ea326b7d7f647ed
tomotakashimizu <tomotaka.developer@gmail.com>
2021-02-12 19:58:19 +0900

    CSS スタイルの適用

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="red">
     {{ text }}
     {{ test }}
   </div>
@@ -16,4 +16,8 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.red {
+  color: red;
+}
+</style>
