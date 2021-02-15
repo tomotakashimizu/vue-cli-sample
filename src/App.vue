47fdc932d30c8ae1fdabe4214ba004eba2c419b0
tomotakashimizu <tomotaka.developer@gmail.com>
2021-02-15 22:23:26 +0900

    コンポーネントを使う方法 のコメントアウト追記

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
+    <!-- 3. 普通の HTML のタグのように Human コンポーネントを書く -->
     <Human />
   </div>
 </template>
 
 <script>
-import Human from "@/components/Human.vue"
+import Human from "@/components/Human.vue" // 1. Human コンポーネントを Human.vue から読み込む
 
 export default {
   components: {
-    Human,
+    Human, // 2. Human コンポーネントを App.vue の中で使えるように登録する
   },
 }
 </script>
