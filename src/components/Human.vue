ba98fec4f2f56048ab2ad2f42a27f05fb19f19bd
tomotakashimizu <tomotaka.developer@gmail.com>
2021-02-15 22:14:56 +0900

    Human コンポーネントを作る

---
+++ src/components/Human.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="human">
+    <div class="atama">
+      <div>😊</div>
+    </div>
+    <div class="karada">
+      <div>💪👕💪</div>
+    </div>
+    <div class="ashi">
+      <div>🦵🦵</div>
+    </div>
+  </div>
+</template>
+
+<style>
+.human {
+  width: 10rem;
+  font-size: 3rem;
+  text-align: center;
+}
+</style>
