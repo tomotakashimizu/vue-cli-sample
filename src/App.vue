d924dd7fb9672d235187fff771de0cf6bb356231
tomotakashimizu <tomotaka.developer@gmail.com>
2021-02-18 18:35:53 +0900

    SimpleMemo コンポーネントを App.vue に追加して使う

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <template>
-  <div class="container">
-    <!-- 3. 普通の HTML のタグのように Human コンポーネントを書く -->
-    <Human />
-    <Human />
-    <Human />
-    <Human />
-    <Human />
+  <div>
+    <div class="container">
+      <!-- 3. 普通の HTML のタグのように Human コンポーネントを書く -->
+      <Human />
+      <Human />
+      <Human />
+      <Human />
+      <Human />
+    </div>
+    <div class="app">
+      <simple-memo />
+    </div>
   </div>
 </template>
 
 <script>
 import Human from "@/components/Human.vue" // 1. Human コンポーネントを Human.vue から読み込む
+import SimpleMemo from "./components/SimpleMemo.vue"
 
 export default {
   components: {
     Human, // 2. Human コンポーネントを App.vue の中で使えるように登録する
-
     // Human: Human, // 省略なしバージョン
     // xxx という変数に入っている値を xxx というプロパティにもつオブジェクトをつくるとき、この省略が使えます。
+
+    SimpleMemo,
   },
 }
 </script>
