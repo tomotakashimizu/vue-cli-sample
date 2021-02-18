bb6e76b6d1bd0a35b6deb1bc9d27030935767c25
tomotakashimizu <tomotaka.developer@gmail.com>
2021-02-18 18:30:39 +0900

    メモとだけ表示する SimpleMemo コンポーネントの作成

---
+++ src/components/SimpleMemo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="simple-memo">
+    メモ
+  </div>
+</template>
+
+<style scoped>
+.simple-memo {
+  margin: 4px;
+  padding: 0.5rem;
+  border-radius: 4px;
+  background-color: orange;
+  color: white;
+}
+</style>
