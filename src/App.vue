2f83a240eaa9a9851cfd75c86f6e2a6eb04e0b30
tomotakashimizu <tomotaka.developer@gmail.com>
2021-02-21 15:07:19 +0900

    slot の例１

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@
       <!-- <simple-memo /> -->
       <simple-memo>メモ１</simple-memo>
       <simple-memo>メモ２</simple-memo>
+      <simple-memo v-for="memo in memos" v-bind:key="memo.id">
+        {{ memo.text }}
+      </simple-memo>
     </div>
   </div>
 </template>
@@ -27,6 +30,14 @@ export default {
     // xxx という変数に入っている値を xxx というプロパティにもつオブジェクトをつくるとき、この省略が使えます。
 
     SimpleMemo,
+  },
+  data() {
+    return {
+      memos: [
+        { id: 0, text: "メモ１" },
+        { id: 1, text: "メモ２" },
+      ],
+    }
   },
 }
 </script>
